cc8f7169bb2660afa03213e808d6615a6824c161
Sohair Nasir <sohairarain002@gmail.com>
2026-03-06 23:57:40 +0500

    Basics Object

---
+++ JS_All_Practice/object/app.js
@@ -0,0 +1,20 @@
+var userName = "Sohair"
+var LastName = "Ullah"
+var age      = 17
+
+var userData = {   //best method for data save
+    userName : "sohan",
+    LastName : "ullah" ,
+    Age      : 20
+ 
+}
+
+// console.log(Age)               // undefind Error
+// console.log(userData)          // full objet show 
+// console.log(userData.userName) // get the specific value
+// console.log(userData.Age="22") // Setting ka new value
+
+var arr = []
+arr.push(userData)
+
+var arr = [['sohan','ullah',20],["Sohair","Ullah",17]] // wrong method ya sahi kaam nahi kary ya value sahi nahi day sakta
